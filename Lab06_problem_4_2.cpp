5a59913d7ca9b3b0e94a5b08927aad25b4c0e288
unknown <oeb.dev1204@gmail.com>
2022-11-11 09:44:47 +0700

    adding lap06_problem_4

--- Lab06_problem_4_2.cpp
+++ Lab06_problem_4_2.cpp
@@ -30,10 +30,15 @@ int main()
     printf("nhap so tien rut: ");
     scanf("%li", &money_withdraw);
 
-    c_50 = money_withdraw / 50000;
-    c_100 = money_withdraw / 100000;
-    c_200 = money_withdraw / 200000;
-    c_500 = money_withdraw / 500000;
+    printf("nhap so to 50000 gioi han: ");
+    scanf("%d", &c_50);
+    printf("nhap so to 100000 gioi han: ");
+    scanf("%d", &c_100);
+    printf("nhap so to 200000 gioi han: ");
+    scanf("%d", &c_200);
+    printf("nhap so to 500000 gioi han: ");
+    scanf("%d", &c_500);
+
 
     for (int wd_50 = 0; wd_50 <= c_50; wd_50++)
         for (int wd_100 = 0; wd_100 <= c_100; wd_100++)
---
+++ Lab06_problem_4_3.cpp
@@ -0,0 +1,41 @@
+/*
+
+Project: Lap_No6 problem 4
+    - Nguyen Gia Bao: Do all of them with the help of his teacher
+
+Aim:
+
+Solution:
+
+Date: 30/10/2022
+
+*/
+
+// Include
+#include <stdio.h>
+#include <conio.h>
+
+// Creating variables
+long money_withdraw;
+int c_50;
+int c_100;
+int c_200;
+int c_500;
+int n = 0;
+
+// Main
+int main()
+{
+
+    printf("nhap so tien rut: ");
+    scanf("%li", &money_withdraw);
+
+    c_500 = money_withdraw / 500000;
+    c_200 = (money_withdraw - c_500 * 500000) / 200000;
+    c_100 = (money_withdraw - c_500 * 500000 - c_200 * 200000) / 100000;
+    c_50 = (money_withdraw - c_500 * 500000 - c_200 * 200000 - c_100 * 100000) / 50000;
+
+    printf("Phuong an toi uu: %d to 50000 | %d to 100000 | %d to 200000 | %d to 500000", c_50, c_100, c_200, c_500);
+
+    return 1;
+}
