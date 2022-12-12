5a8c6a1519267d9273b9dcf3bdfc70cca8a77e81
Bao Kun <oeb.dev1204@gmail.com>
2022-12-13 00:09:46 +0700

    change

--- Lap10_problem_01.c
+++ Lap10_problem_01.c
@@ -1,6 +1,6 @@
 /*
 
-Project: Lab09_problem_05
+Project: Lab10_problem_2
     - Nguyen Gia Bao: Do all of them with the help of his teacher
 
 Aim:
@@ -16,48 +16,32 @@ Date: 23/07/2004
 #include <stdio.h>
 #include <stdlib.h>
 
-// Creating define 
+// Creating define
 // #define RAND_MAX 50
 
 // Creating functions
-void array_update(int *arr,int length);
+void array_update(int *arr, int length);
 void array_show(int *arr, int length, int per_row);
+void problem_1_1();
+void problem_1_2();
+void problem_1_3();
 
 // Creating variables
-int n;
-int element_per_row;
 
 // Main
 int main()
 {
-    // // 1.1
-    // printf("Pls enter value of n: ");
-    // scanf("%d", &n);
-    // int array_int[n];
-    // array_update(&array_int, n);
-    // printf("Enter element per row that you want to display: ");
-    // scanf("%d", &element_per_row);
-    // array_show(&array_int, n, element_per_row);
-    // // 1.2
-    // printf("Pls enter value of n: ");
-    // scanf("%d", &n);
-    // int array_b[n];
-    // for (int i = 0; i < n; i++)
-    // {
-    //     array_b[i] = rand() % 50;
-    //     printf("%d ", array_b[i]);
-    // }
-
+    problem_1_1();
+    problem_1_2();
+    problem_1_3();
     return 1;
 }
 
-
-
-void array_update(int *arr,int length)
+void array_update(int *arr, int length)
 {
     for (int i = 0; i < length; i++)
     {
-        printf("Pls enter value of element %d: ", i);
+        printf("Pls enter value of element %d: ", i + 1);
         scanf("%d", &arr[i]);
     }
 }
@@ -78,4 +62,72 @@ void array_show(int *arr, int length, int per_row)
         }
         printf("\n");
     }
+}
+
+void problem_1_1()
+{
+    printf("Problem 1.1\n");
+    int n;
+    int element_per_row;
+    printf("Pls enter value of n: ");
+    scanf("%d", &n);
+    int array_int_1_1[n];
+    array_update(array_int_1_1, n);
+    printf("Enter element per row that you want to display: ");
+    scanf("%d", &element_per_row);
+    array_show(array_int_1_1, n, element_per_row);
+}
+
+void problem_1_2()
+{
+    printf("\nProblem 1.2\n");
+    int n;
+    int sum;
+    printf("Pls enter value of n: ");
+    scanf("%d", &n);
+    int array_1_3[n];
+    for (int i = 0; i < n; i++)
+    {
+        array_1_3[i] = rand() % 50;
+        sum += array_1_3[i];
+        printf("%d ", array_1_3[i]);
+    }
+    printf("\nSum of array: %d\n", sum);
+}
+void problem_1_3()
+{
+    printf("\nProblem 1.3\n");
+    int n;
+    int a;
+    int b;
+    float temp;
+    printf("Pls enter value of n: ");
+    scanf("%d", &n);
+    printf("Pls enter value of a: ");
+    scanf("%d", &a);
+    printf("Pls enter value of b: ");
+    scanf("%d", &b);
+    float array_1_3[n];
+    temp = (a + b) / 2;
+
+    for (int i = 0; i < n;)
+    {
+        printf("Value of element %d: ", i + 1);
+        scanf("%f", &temp);
+        if (temp >= a && temp <= b)
+        {
+            array_1_3[i] = temp;
+            i++;
+        }
+        else
+        {
+            printf("dude pls enter value between %d and %d\n", a, b);
+        }
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        printf("%.2f ", array_1_3[i]);
+    }
+
 }
